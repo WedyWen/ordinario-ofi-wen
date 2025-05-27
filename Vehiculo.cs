0eeeea7ad246ab8ed94f45342a05c5177eefccb0
wendy <wendy@DESKTOP-5HSMVIR>
2025-05-27 03:53:50 -0600

    1

--- Vehiculo.cs
+++ Vehiculo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,20 +9,16 @@ namespace ordinario_ofi_wen
 {
     internal class Vehiculo
     {
-        public Vehiculo(int id, string marca, string modelo, int anio, string color)
-        {
-            Id = id;
-            Marca = marca;
-            Modelo = modelo;
-            Anio = anio;
-            Color = color;
-        }
-        public Vehiculo() { }
 
-        public int Id { get; set; }
-        public string Marca { get; set; }
-        public string Modelo { get; set; }
-        public int Anio { get; set; }
-        public string Color { get; set; }
+        public Vehiculo(double precio, string estado)
+        {
+            Precio = precio;
+            Estado = estado;
+        }
+
+        public double Precio { get; set; }
+        public string Estado { get; set; }
+
+        public Vehiculo() { }
     }
 }
